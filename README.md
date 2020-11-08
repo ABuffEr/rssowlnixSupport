23f7ef8b9ade0aab746f9539f38f17c38d40d49d
ABuffEr <a.buffolino@gmail.com>
2020-11-08 11:58:31 +0100

    2.0 release: added announcement of read/unread status of news.

--- README.md
+++ README.md
@@ -2,14 +2,16 @@
 
 A NVDA add-on to improve RSSOwlnix accessibility.
 
+It includes the announcement read/unread status of news according to default bolding style.
+
 For information about RSSOwlnix, an updated version of famous RSSOwl feed reader, see [here.][1]
 
 To get the add-on, download it from [here.][2]
 
 # Technical info
 
-This is an extremely straightforward add-on: it simply imports the great work done by NVDA for Eclipse, which RSSOwlnix is based on.
+This is an extremely straightforward add-on: it simply imports the great work done by NVDA for Eclipse, which RSSOwlnix is based on, adding just a class for read/unread status of news.
 
 
 [1]: https://github.com/Xyrio/RSSOwlnix
-[2]: https://raw.githubusercontent.com/ABuffEr/rssowlnixSupport/master/packages/rssowlnixSupport-1.0.nvda-addon
+[2]: https://raw.githubusercontent.com/ABuffEr/rssowlnixSupport/master/packages/rssowlnixSupport-2.0.nvda-addon
--- addon/appModules/rssowlnix.py
+++ addon/appModules/rssowlnix.py
@@ -1,7 +1,46 @@
-﻿# -*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 # Author: Alberto Buffolino
 # Original license of imported module, see:
 # https://github.com/nvaccess/nvda/blob/master/source/appModules/eclipse.py
 
+from nvdaBuiltin.appModules.eclipse import *
+from addonHandler import initTranslation
+from displayModel import DisplayModelTextInfo
+from NVDAObjects.IAccessible.sysTreeView32 import TreeViewItem
+import controlTypes as ct
+import textInfos
 
-from appModules.eclipse import *
+initTranslation()
+
+unreadLabel = _("Unread, ")
+
+class AppModule(AppModule):
+
+	def __init__(self, processID, appName=None):
+		super(AppModule, self).__init__(processID, appName)
+
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		if obj.role == ct.ROLE_TREEVIEWITEM and obj.windowStyle == 1445057189:
+			clsList.insert(0, NewsItem)
+
+class NewsItem(TreeViewItem):
+
+	def isUnread(self):
+		info = DisplayModelTextInfo(self, position=textInfos.POSITION_FIRST)
+		info.expand(textInfos.UNIT_CHARACTER)
+		fields = info.getTextWithFields()
+		try:
+			bold = fields[0].field["bold"]
+		except:
+			bold = False
+		return bold
+
+	def event_gainFocus(self):
+		if self.name == super().name and self.isUnread():
+			self.name = unreadLabel+self.name
+		else:
+			self.name = super().name
+		super(NewsItem, self).event_gainFocus()
+
+	def event_typedCharacter(self):
+		self.event_gainFocus()
